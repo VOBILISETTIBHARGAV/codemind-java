b243e62a0efeaee6b7f4147015d8b9854a885270
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2023-01-31 11:47:06 +0530

    import java.util.*; class add {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,v;         a=sc.nextInt();         b=sc.nextInt();         v=a+b;         System.out.println(v);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class add
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,v;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        v=a+b;
+        System.out.println(v);
+    }
+}
